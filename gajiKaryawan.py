e6094ee8501819e2a0d70dcfcaa4947f7daa08c1
sundinugraha12345-a11y <sundinugraha12345@gmail.com>
2026-03-04 16:16:32 +0700

    update

--- gajiKaryawan.py
+++ gajiKaryawan.py
@@ -1,13 +1,13 @@
-GajiPokok = 5000000
-Tunjangan = 750000
+Gajipokok  = 5000000
+Tunjangan  = 750000
 
-GajiKotor = GajiPokok + Tunjangan
+GajiKotor = Gajipokok + Tunjangan
 
 PotonganPajak = 5 / 100
 PotonganBPJS = 2 / 100
-TotalPersen = PotonganPajak + PotonganBPJS
+Persen = PotonganPajak + PotonganBPJS
 
-TotalPotongan = GajiKotor * TotalPersen
+TotalPotongan = GajiKotor * Persen
 
 GajiBersih = GajiKotor - TotalPotongan
-print("total gaji karyawan RP: ", GajiBersih)
+print("total bersih gaji karyawan RP:",GajiBersih)
