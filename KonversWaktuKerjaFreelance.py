92768d24776d7d45b34eaba37ea32b1dfe1be915
sundinugraha12345-a11y <sundinugraha12345@gmail.com>
2026-03-04 19:38:04 +0700

    tester 3

---
+++ KonversWaktuKerjaFreelance.py
@@ -0,0 +1,11 @@
+jam = 5
+menit = 45
+total_menit = (jam * 60) + menit
+
+print("Total menit kerja freelance: ", total_menit, "menit")
+
+upah_per_jam = 85000
+total_upah = (total_menit / 60) * upah_per_jam
+
+print("Total upah freelance: Rp", total_upah)
+
